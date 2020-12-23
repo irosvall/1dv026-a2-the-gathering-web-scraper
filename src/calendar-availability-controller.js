0bbc09cc85dfff8132aaceccd5462786ca146825
Ida Rosvall <ir222gn@student.lnu.se>
2020-12-23 12:17:27 +0100

    Change that calander availability controller actually returns days

--- src/calendar-availability-controller.js
+++ src/calendar-availability-controller.js
@@ -18,20 +18,34 @@ export class CalendarAvailabilityController {
    * Checks which day(s) all calendars are available.
    *
    * @param {string} url - A url that contains links to people's calendars.
-   * @returns {string[]} List containing which day(s) are available.
+   * @returns {string[]} List containing which weekend day(s) are available.
    */
   static async checkAvailableDays (url) {
     const calendarList = await LinkScraper.scrapeWebsiteLinks(url)
 
     // Gets the first calendar to compare.
-    const daysAvailable = await CalendarAvailabilityScraper.scrapeIfAvailable(calendarList[0])
+    const daysOk = await CalendarAvailabilityScraper.scrapeIfAvailable(calendarList[0])
 
     // Compare the other's calendars with the first calendar. If a day doesn't work then 'ok' is changed.
     for (let i = 1; i < calendarList.length; i++) {
-      const tempDaysAvailable = await CalendarAvailabilityScraper.scrapeIfAvailable(calendarList[i])
-      for (let j = 0; j < tempDaysAvailable.length; j++) {
-        if (tempDaysAvailable[j] !== 'ok') {
-          daysAvailable[j] = tempDaysAvailable[j]
+      const tempdaysOk = await CalendarAvailabilityScraper.scrapeIfAvailable(calendarList[i])
+      for (let j = 0; j < tempdaysOk.length; j++) {
+        if (tempdaysOk[j] !== 'ok') {
+          daysOk[j] = tempdaysOk[j]
+        }
+      }
+    }
+
+    // Translates the ok days to weekend days. The available days gets pushed into list.
+    const daysAvailable = []
+    for (let i = 0; i < daysOk.length; i++) {
+      if (daysOk[i] === 'ok') {
+        if (i === 0) {
+          daysAvailable.push('friday')
+        } else if (i === 1) {
+          daysAvailable.push('saturday')
+        } else if (i === 2) {
+          daysAvailable.push('sunday')
         }
       }
     }
